1ddfa190041f47b5c2dfeb8df2bd7a7251a7ec41
Furkan Baris Duyar <furkanbarisduyar@outlook.com>
2025-02-05 05:26:11 +0000

    Add room type to item

--- DES502_Game_Prototype/Assets/Scripts/Item/Item.cs
+++ DES502_Game_Prototype/Assets/Scripts/Item/Item.cs
@@ -1,8 +1,10 @@
+using Globals;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    
+    public ROOM_TYPE[] roomTypes;
+
 }
